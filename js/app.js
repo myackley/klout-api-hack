714f533ef6ca6f8a21db27eeeafbf936e999e3e2
Mark Yackley <myackley@hotmail.com>
2015-01-04 19:49:18 -0600

    Modified structure to have empty 'boxes' for the two user areas to load templates into, added functionality to load 'entered' user templates into the two 'box' areas and check for Klout Id and past the JSON data to another function to get the Klout Score

--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
 $(function() {
 
 	var apiKey = "gctmf4efq2wpj5c5q5z5hsca";
+	var resultsArea = $(".kloutapp-results"); // get results area
+	var kloutId = "";
 
 	// display 'empty' layouts
 	var loadDefaultLayouts = function() {
-		// get results area
-		var resultsArea = $(".kloutapp-results");
 		// load it
-		for (i=0;i<2;i++) {
+		var boxes = $(".kloutapp-results .results-box");
+		$.each(boxes, function(){
 			var emptyLayout = $(".template .results-empty").clone();
-			resultsArea.append(emptyLayout);
-		}
+			$(this).append(emptyLayout);
+		});
 	};
-
 	// init 'empty' layouts
 	loadDefaultLayouts();
 
+
+
+
+
 	$("button").on("click",function(event) {
 		event.preventDefault();
-		// var current = $(this).parents(".results-empty");
+		var current = $(this).parents(".results-box");
 		// grab tw handle
-		var twHandle = $("input[type='text'").val();
-		displayData(twHandle);
+		var twHandle = current.find("input").val();
+		displayData(current,twHandle);
 	});
 
 	$(".close-btn").on("click",function() {
 		// remove user and default back to 'empty' layout
 	});
 
-	var displayData = function(handle) {
-		// get user & load into layout
-		getUser(handle);
-		// get score & load into layout
-		//console.log("get score");		
-		// get topics & load into layout
-		//console.log("get topics");
-	};
 
-	var getUser = function(handle) {
+
+
+
+
+
+	var displayData = function(current,handle) {
+		// get user & load into layout
 		var request = {
 			screenName:handle,
 			key:apiKey
@@ -49,21 +51,43 @@ $(function() {
 			dataType: "jsonp",
 			type: "GET",
 		})
-		.success(function(result){
+		.done(function(result){
+			var kloutId = result.id;
+			// clear error message... $(".error-messages").hide().find("p").text("");
+			
+			current.empty();
+			var enteredLayout = $(".template .results-entered").clone();
 
-			$(".error-messages").hide().find("p").text("");
-			console.log(result.id);
+			var handleHref = enteredLayout.find(".results-header p > a");
+			var handleName = enteredLayout.find(".results-header p");
+			handleName.attr("href","http://www.klout.com/"+kloutId);
+			handleName.text("@"+handle);
+
+			current.append(enteredLayout);
+
+			getScore(current,kloutId);
 
 		})
-		.error(function(jqXHR, error, errorThrown){
-			console.log(jqXHR.readyState);
-			console.log(jqXHR.status);
-			console.log(jqXHR.responseText);
-			console.log(error);
-			console.log(errorThrown);
-			// var errorElem = showError(error);
+		.fail(function(jqXHR, error, errorThrown){
+
 			// $(".error-messages").show().find("p").append(errorElem);
 		});
 	};
+
+
+
+	var getScore = function(current,kloutId) {
+		// get score & load into layout
+		console.log("get score");
+		console.log(current);
+		console.log(kloutId);
+	};
+
+
+
+	// get topics & load into layout
+	//console.log("get topics");
+
+
 
 });
